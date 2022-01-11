811105f2bf300242b1ac77a59599cc8402c2bfe9
masa9380 <def.aa33@gmail.com>
2022-01-11 22:06:16 +0900

    complete enumerate and list_comprehension

---
+++ enumerate.py
@@ -0,0 +1,10 @@
+# enumerate リスト内各要素の値とインデックスを取得する
+fruits = ["apple", "peach", "grapes", "banana"]
+
+# for idx, fruit in enumerate(fruits):
+#     print(idx)
+#     print(fruit)
+
+for x in enumerate(fruits):
+    print(x)
+
